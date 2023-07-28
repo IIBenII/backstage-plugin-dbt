41b428de39da31072254e70513f909bc7eae585f
Benjamin Berriot <40873921+IIBenII@users.noreply.github.com>
2023-07-28 18:39:24 +0200

    fix: isDBTAvailable not working when global bucket is set (#108)

--- packages/dbt/src/plugin.ts
+++ packages/dbt/src/plugin.ts
@@ -2,13 +2,25 @@ import { rootRouteRef } from './routes';
 import {
   createPlugin,
   createRoutableExtension,
+  useApi,
+  configApiRef,
 } from '@backstage/core-plugin-api';
+
 
 import { useEntity } from '@backstage/plugin-catalog-react';
 
 export const isDBTAvailable = () => {
   const { entity } = useEntity();
-  return Boolean(entity.metadata.annotations?.["dbtdoc-bucket"]);
+  const config = useApi(configApiRef);
+  if (entity.metadata.annotations?.["dbtdoc-bucket"] != null) {
+    return true;
+  }
+  else if (config.getOptionalString('dbtdoc.bucket')) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 export const dbtPlugin = createPlugin({
