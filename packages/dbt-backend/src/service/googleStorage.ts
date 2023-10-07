6814c77e47eb6847bf8a3832ddda3a0fae1c5272
Mattias Sjödin <mattias.sjodin@northvolt.com>
2023-10-07 18:36:03 +0200

    feat: add googleStorage.ts

---
+++ packages/dbt-backend/src/service/googleStorage.ts
@@ -0,0 +1,20 @@
+import { Storage } from '@google-cloud/storage';
+
+/**
+ * GoogleStorageProvider is a class that provides functionality for downloading files
+ * from Google Cloud Storage.
+ */
+export class GoogleStorageProvider {
+  private storage = new Storage();
+
+  /**
+   * Downloads a file from Google Cloud Storage.
+   * @param bucket - The name of the storage bucket.
+   * @param filePath - The path to the file within the bucket.
+   * @returns A Promise that resolves to the file's contents as a Buffer.
+   */
+  async downloadFile(bucket: string, filePath: string): Promise<Buffer> {
+    const [contents] = await this.storage.bucket(bucket).file(filePath).download();
+    return contents;
+  }
+}
